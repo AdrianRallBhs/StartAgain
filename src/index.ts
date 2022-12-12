8c1c6ebb1c9c23cda35a6bc5e0d3903f18f84676
Adrian Rall <adrian@asda4.de>
2022-12-12 13:44:41 +0100

    core.path

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ async function execute(): Promise<void> {
         const projectIgnoreList = core.getMultilineInput("ignore-project").filter(s => s.trim() !== "")
         const contents = core.getInput("contents", { required: true });
         const mode = (core.getInput("write-mode") || "append").toLocaleLowerCase();
+        const path = core.getInput("path", { required: true });
     
 
         core.startGroup("Find modules")
