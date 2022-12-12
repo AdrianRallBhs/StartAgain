7bdfca1c0d2959b20903b686b87aeb632c9adc8a
Adrian Rall <adrian@asda4.de>
2022-12-12 14:19:10 +0100

    toString?

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ async function execute(): Promise<void> {
                 core.endGroup()
 
                 core.startGroup(`add to README`)
-                await updateReadme(dotnet.listOutdated)
+                await updateReadme(dotnet.listOutdated.toString())
                 core.endGroup()
 
                 core.startGroup(`append to PR body  ${project}`)
