1093492283a57582bca310ea8664bd9b055cab5a
Adrian Rall <adrian@asda4.de>
2023-02-15 09:03:57 +0100

    ++ udpa

--- src/write-in-repo.ts
+++ src/write-in-repo.ts
@@ -1,28 +1,47 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fetch = require('node-fetch');
 
-export async function writeInRepo() {
-  try {
-    // Generate the markdown content
-    const markdownContent = `# Output
-    - Item 1
-    - Item 2`;
+export async function writeInRepo(inputText: any) {
+    try {
+        // Generate the markdown content
+        const markdownContent = `${inputText}`;
 
-    // Get authenticated instance of the GitHub API
-    const token = process.env.TOKEN;
-    const octokit = github.getOctokit(token);
+        // Construct the API URL
+        const baseUrl = `https://api.github.com`;
+        const owner = 'AdrianRallBhs';
+        const repo = 'submarine';
+        const filePath = 'README.md';
+        const url = `${baseUrl}/${owner}/${repo}/${filePath}`;
 
-    // Create or update the markdown file in the target repository
-    const result = await octokit.repos.createOrUpdateFileContents({
-      owner: 'AdrianRallBhs',
-      repo: 'submarine',
-      path: 'README.md',
-      message: 'Added output to markdown file',
-      content: Buffer.from(markdownContent).toString('base64')
-    });
+        // Create the request body
+        const body = {
+            message: 'Added output to markdown file',
+            content: Buffer.from(markdownContent).toString('base64'),
+        };
 
-    console.log(result);
-  } catch (e) {
-    core.setFailed(e);
-  }
+        // Set the request headers
+        const token = process.env.TOKEN;
+        const headers = {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            Accept: 'application/vnd.github.v3+json',
+        };
+
+        // Send the request to create or update the file
+        const response = await fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify(body),
+            headers: headers,
+        });
+
+        // Check the response status code
+        if (response.status === 200 || response.status === 201) {
+            console.log('File created or updated successfully.');
+        } else {
+            const data = await response.json();
+            console.error('Failed to create or update file:', data.message);
+        }
+    } catch (e) {
+        core.setFailed(e);
+    }
 }
