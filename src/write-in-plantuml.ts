13c1f8cb8664dabc280f6b3cc4ec5ac26781bc9c
Adrian Rall <adrian@asda4.de>
2023-02-16 15:53:33 +0100

    sfjssdf

--- src/write-in-plantuml.ts
+++ src/write-in-plantuml.ts
@@ -1,9 +1,9 @@
 import fs from 'fs';
 
 interface Library {
-  DependencyName: string;
-  Version: string;
-  parent: string | null;
+    DependencyName: string;
+    Version: string;
+    parent: string | null;
 }
 
 const lockJson = require('../package-lock.json'); // edit path if needed
@@ -11,41 +11,41 @@ const lockJson = require('../package-lock.json'); // edit path if needed
 export let plantumlString: string = "";
 const libraries: any[] = [];
 
-  Object.keys(lockJson.dependencies).forEach((dependencyName) => {
+Object.keys(lockJson.dependencies).forEach((dependencyName) => {
     const dependencyData = lockJson.dependencies[dependencyName];
 
     libraries.push({
-      ChildDependencyName: dependencyName.toString(),
-      ChildVersion: dependencyData.version.toString(),
-      parent: null,
+        ChildDependencyName: dependencyName.toString(),
+        ChildVersion: dependencyData.version.toString(),
+        parent: null,
     });
 
     if (dependencyData.requires) {
-      Object.keys(dependencyData.requires).forEach((subdependencyName) => {
-        const subdependencyVersion = dependencyData.requires[subdependencyName];
+        Object.keys(dependencyData.requires).forEach((subdependencyName) => {
+            const subdependencyVersion = dependencyData.requires[subdependencyName];
 
-        libraries.push({
-          ParentDependencyName: subdependencyName.toString(),
-          ParentVersion: subdependencyVersion.toString(),
-          parent: dependencyName.toString(),
+            libraries.push({
+                ParentDependencyName: subdependencyName.toString(),
+                ParentVersion: subdependencyVersion.toString(),
+                parent: dependencyName.toString(),
+            });
         });
-      });
     }
-  });
+});
 
-  plantumlString += '@startuml\n';
-  plantumlString += 'digraph foo {\n';
+plantumlString += '\n@startuml\n';
+plantumlString += 'digraph foo {\n';
 
-  libraries.forEach((library) => {
+libraries.forEach((library) => {
     if (library.parent) {
-      plantumlString += `"${library.ParentDependencyName} ${library.ChildVersion}" -> "${library.parent} ${library.Version}"\n`;
+        plantumlString += `"${library.ParentDependencyName} ${library.ParentVersion}" -> "${library.ChildDependencyName} ${library.ChildVersion}"\n`;
     } else {
-    //   plantumlString += `"package ${library.DependencyName} ${library.Version}" {\n`;
-    //   const subdependencies = libraries.filter((sub) => sub.parent === library.DependencyName);
-    //   subdependencies.forEach((sub) => {
-    //     plantumlString += `\t${sub.DependencyName} : ${sub.Version}\n`;
-    //   });
+        //   plantumlString += `"package ${library.DependencyName} ${library.Version}" {\n`;
+        //   const subdependencies = libraries.filter((sub) => sub.parent === library.DependencyName);
+        //   subdependencies.forEach((sub) => {
+        //     plantumlString += `\t${sub.DependencyName} : ${sub.Version}\n`;
+        //   });
     }
-  });
+});
 
-  plantumlString += '}\n@enduml\n';
+plantumlString += '}\n@enduml\n';
