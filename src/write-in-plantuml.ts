a3b0701aeb72efd00e263400915926403fa3072e
Adrian Rall <adrian@asda4.de>
2023-02-16 20:07:48 +0100

    new write plantuml class

--- src/write-in-plantuml.ts
+++ src/write-in-plantuml.ts
@@ -52,35 +52,70 @@
 
 
 
-import fs from 'fs';
-
 interface Library {
     DependencyName: string;
     Version: string;
     parent: string | null;
-}
-
-const lockJson = require('../package-lock.json'); // edit path if needed
-
-export let plantumlString: string = "";
-const libraries: any[] = [];
-const latestVersions: {[key: string]: string} = {}; // dictionary to store latest versions
-
-// function to check if a version is greater than another, accounting for caret sign
-function isVersionGreaterThan(version: string, otherVersion: string): boolean {
-    if (version.startsWith('^') && otherVersion.startsWith('^')) {
-        const versionNumber = Number(version.substring(1));
-        const otherVersionNumber = Number(otherVersion.substring(1));
-        return versionNumber > otherVersionNumber;
-    } else if (version.startsWith('^')) {
-        const versionNumber = Number(version.substring(1));
-        const otherVersionNumber = Number(otherVersion.split('.')[0]);
-        return versionNumber > otherVersionNumber;
-    } else if (otherVersion.startsWith('^')) {
-        const versionNumber = Number(version.split('.')[0]);
-        const otherVersionNumber = Number(otherVersion.substring(1));
-        return versionNumber > otherVersionNumber;
-    } else {
-        return version > otherVersion;
+  }
+  
+  const lockJson = require('../package-lock.json'); // edit path if needed
+  
+  function areVersionsCompatible(version1: string, version2: string): boolean {
+    const version1Parts = version1.split(".");
+    const version2Parts = version2.split(".");
+    for (let i = 0; i < Math.max(version1Parts.length, version2Parts.length); i++) {
+      const v1 = parseInt(version1Parts[i] || "0");
+      const v2 = parseInt(version2Parts[i] || "0");
+      if (v1 !== v2) {
+        return v1 > v2;
+      }
     }
-}
+    return true;
+  }
+  
+  let plantumlString: string = "";
+  const libraries: any[] = [];
+  
+  Object.keys(lockJson.dependencies).forEach((dependencyName) => {
+    const dependencyData = lockJson.dependencies[dependencyName];
+  
+    libraries.push({
+      DependencyName: dependencyName.toString(),
+      Version: dependencyData.version.toString(),
+      parent: null,
+    });
+  
+    if (dependencyData.requires) {
+      Object.keys(dependencyData.requires).forEach((subdependencyName) => {
+        const subdependencyVersion = dependencyData.requires[subdependencyName];
+  
+        if (areVersionsCompatible(dependencyData.version, subdependencyVersion)) {
+          libraries.push({
+            DependencyName: subdependencyName.toString(),
+            Version: subdependencyVersion.toString(),
+            parent: dependencyName.toString(),
+          });
+        } else {
+          const parent = libraries.find((lib) => lib.DependencyName === subdependencyName);
+          libraries.push({
+            DependencyName: subdependencyName.toString(),
+            Version: subdependencyVersion.toString(),
+            parent: parent?.DependencyName || null,
+          });
+        }
+      });
+    }
+  });
+  
+  plantumlString += '\n@startuml\n';
+  plantumlString += 'digraph foo {\n';
+  
+  libraries.forEach((library) => {
+    const subdependencies = libraries.filter((sub) => sub.parent === library.DependencyName);
+    subdependencies.forEach((sub) => {
+      plantumlString += `"${sub.DependencyName} ${sub.Version}" -> "${library.DependencyName} ${library.Version}"\n`;
+    });
+  });
+  
+  plantumlString += '}\n@enduml\n';
+  
